0c7c17de90b7678134bb2b5a2bf0e53c7a95abbb
SukachevAlex <sukachevalexandr@gmail.com>
2019-02-25 13:43:49 +0300

    Add fonts to project
    Add glober thin, regular, bold font

--- first/src/App.tsx
+++ first/src/App.tsx
@@ -1,10 +1,17 @@
+import { cn } from '@bem-react/classname';
 import React, { Component } from 'react';
+
 import './App.css';
+import './fonts/fonts.css';
+
+const cnPage = cn('Page');
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div className={cnPage()}>
+      123
+      </div>
     );
   }
 }
